2e1cd995ff5428af7639877dfe79fe67bb3dc5a9
Newtayo <akandeabdul@gmail.com>
2023-06-27 17:13:51 +0100

    Set up login and registration page using device

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
+  root 'users#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
